c8a9cbd6ed4f1b5bd1beb1d9c6e2e61dba21386b
ecee <you@example.com>
2023-09-23 20:26:47 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(peopleName , eventName){
+   const peopleName = ["Jared","Samson","Annette"];
+   const eventName =["Poolparty"]
+   for(let i = 0; i<peopleName.length; i++){
+    const message = `Thank you, ${peopleName[i]}, for attending the ${eventName} event.`;
+
+}
+}
+let countDown = 5;
+while(countDown < 10){
+    console.log(countDown)
+    countDown --;
+}
